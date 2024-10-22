14c844a181a69cec4f7633d80fb14337c2a2abb8
sgarnica1 <sgarnica1902@gmail.com>
2024-10-23 00:08:43 +0200

    feat(google): add google auth

--- driver-app/app/src/main/java/com/salbox/driverapp/ui/view/LoginActivity.kt
+++ driver-app/app/src/main/java/com/salbox/driverapp/ui/view/LoginActivity.kt
@@ -2,58 +2,100 @@ package com.salbox.driverapp.ui.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.firebase.auth.FirebaseAuth
-import com.salbox.driverapp.BuildConfig
+import com.google.firebase.auth.GoogleAuthProvider
 import com.salbox.driverapp.R
 import com.salbox.driverapp.ui.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var auth: FirebaseAuth
-
-    private lateinit var verifyCodeInput: TextInputLayout
-    private lateinit var verifyButton: Button
-
-
-    private var accessCode = BuildConfig.ACCESS_C0DE
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var googleSignInButton: SignInButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen() // Display the splash screen
         setContentView(R.layout.activity_login)
 
-        loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        loginViewModel = ViewModelProvider(this)[(LoginViewModel::class.java)]
 
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
+        initializeGoogleSignInClient()
         initializeViews()
         initializeClickListeners()
     }
 
+    private fun initializeGoogleSignInClient() {
+        // Configure Google Sign-In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) // Your web client ID from Firebase
+            .requestEmail()
+            .build()
 
-    private fun signIn() {
-        val code = verifyCodeInput.editText?.text.toString().trim()
-        if (code.isNotEmpty() && code == accessCode) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        } else
-            showToast(getString(R.string.invalid_verification_code))
-
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
+
     private fun initializeViews() {
-        verifyCodeInput = findViewById(R.id.verifyCodeInput)
-        verifyButton = findViewById(R.id.verifyButton)
+        googleSignInButton = findViewById(R.id.googleSignInButton)
     }
 
     private fun initializeClickListeners() {
-        verifyButton.setOnClickListener { signIn() }
+        googleSignInButton.setOnClickListener { signInWithGoogle() }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+    }
+
+    // Launcher for handling Google Sign-In result
+    private val googleSignInLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if (task.isSuccessful) {
+                val account = task.result
+                account?.let { firebaseAuthWithGoogle(it) }
+                loginViewModel.setUser(account)
+            } else {
+                showToast(getString(R.string.google_sigin_error))
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = FirebaseAuth.getInstance().currentUser
+                    if(user != null) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        showToast(getString(R.string.sigin_error))
+                    }
+
+                } else {
+                    showToast(getString(R.string.sigin_error))
+                }
+            }
     }
 
     private fun showToast(message: String) {
