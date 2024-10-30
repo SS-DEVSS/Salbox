5d427d33e56ecfe7043b72e306b477212c9e90c9
FloresTM <seb.flores2002@gmail.com>
2024-10-31 03:17:33 +1100

    grid

--- salbox-landing/src/modules/AppPromotion.tsx
+++ salbox-landing/src/modules/AppPromotion.tsx
@@ -5,14 +5,15 @@ import AppleIcon from "../assets/images/AppleIcon.png";
 
 export function AppPromotion() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center md:gap-16 px-6 xl:px-20 2xl:px-40 overflow-hidden md:max-h-[900px]">
+    <div className="flex flex-col md:flex-row items-center justify-center md:gap-16 px-6 xl:px-20 2xl:px-40 overflow-hidden relative bg-white md:max-h-[900px] bg-grid-small-black/[0.2]">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <ContainerScroll
         titleComponent={
           <div className="md:hidden">
-            <h2 className="text-4xl uppercase font-semibold text-scarlet-400 dark:text-white">
+            <h2 className="text-4xl uppercase font-semibold text-scarlet-400">
               ¡Proximamente! <br />
             </h2>
-            <h1 className="text-4xl  md:text-[6rem] font-bold mt-5 leading-none">
+            <h1 className="text-4xl md:text-[6rem] font-bold mt-5 leading-none">
               Salbox Go App
             </h1>
             <p className="mt-6 text-base leading-9">
@@ -30,12 +31,13 @@ export function AppPromotion() {
           className="mx-auto rounded-2xl object-cover h-full"
         />
       </ContainerScroll>
+
       <div className="md:hidden">
         <h3 className="font-bold text-lg my-6 text-center">
           ¿En dondé la podré encontrar?
         </h3>
         <div className="flex flex-col gap-2 mb-20">
-          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[##A4A4A4] bg-black">
+          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[#A4A4A4] bg-black">
             <img src={AppleIcon} className="w-14 h-14" alt="apple img" />
             <div>
               <p className="uppercase text-white text-lg">Disponible en</p>
@@ -44,7 +46,7 @@ export function AppPromotion() {
               </p>
             </div>
           </div>
-          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[##A4A4A4] bg-black">
+          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[#A4A4A4] bg-black">
             <img
               src={GooglePlayIcon}
               className="w-14 h-14"
@@ -59,8 +61,9 @@ export function AppPromotion() {
           </div>
         </div>
       </div>
+
       <div className="hidden md:block">
-        <h2 className="text-4xl uppercase font-semibold text-scarlet-400 dark:text-white">
+        <h2 className="text-4xl uppercase font-semibold text-scarlet-400">
           ¡Proximamente! <br />
         </h2>
         <h1 className="text-7xl xl:text-[6rem] font-bold mt-1 leading-none">
@@ -76,7 +79,7 @@ export function AppPromotion() {
           ¿En dondé la podré encontrar?
         </h3>
         <div className="flex flex-col flex-wrap lg:flex-row gap-2">
-          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[##A4A4A4] bg-black">
+          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[#A4A4A4] bg-black">
             <img src={AppleIcon} className="w-14 h-14" alt="apple img" />
             <div>
               <p className="uppercase text-white text-md">Consíguelo en el</p>
@@ -85,7 +88,7 @@ export function AppPromotion() {
               </p>
             </div>
           </div>
-          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[##A4A4A4] bg-black">
+          <div className="flex items-center gap-6 p-4 px-8 rounded-lg border border-[#A4A4A4] bg-black">
             <img
               src={GooglePlayIcon}
               className="w-14 h-14"
